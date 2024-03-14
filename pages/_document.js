a8bba93829a2dd1f1a15905f436ce7f1d27cd6bc
hadi-azeez <hadi.azeez@nutrien.com.au>
2024-03-15 02:16:11 +0400

    another fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,31 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Document from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Helmet } from "react-helmet";
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
+    const helmet = Helmet.renderStatic();
     return (
       <Html lang="en">
         <Head>
+          {helmet.title.toComponent()}
+          {helmet.meta.toComponent()}
           <meta
-            httpEquiv="Content-Security-Policy"
+            http-equiv="Content-Security-Policy"
             content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';"
           />
-          <meta httpEquiv="X-Content-Type-Options" content="nosniff" />
-          <meta httpEquiv="X-Frame-Options" content="SAMEORIGIN" />
-          <meta httpEquiv="X-XSS-Protection" content="1; mode=block" />
+          <meta http-equiv="X-Content-Type-Options" content="nosniff" />
+          <meta http-equiv="X-Frame-Options" content="SAMEORIGIN" />
+          <meta http-equiv="X-XSS-Protection" content="1; mode=block" />
           <meta
-            httpEquiv="Strict-Transport-Security"
+            http-equiv="Strict-Transport-Security"
             content="max-age=31536000; includeSubDomains; preload"
           />
+          {/* Add other headers if necessary */}
         </Head>
         <body>
           <Main />
@@ -26,3 +35,5 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+export default MyDocument;
