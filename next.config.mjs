7c463a45f7af7884adfc145064279b5292df6120
hadi-azeez <hadi.azeez@nutrien.com.au>
2024-03-15 02:49:00 +0400

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -12,10 +12,6 @@ const nextConfig = {
             value: "SAMEORIGIN",
           },
           {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'",
-          },
-          {
             key: "X-Content-Type-Options",
             value: "nosniff",
           },
