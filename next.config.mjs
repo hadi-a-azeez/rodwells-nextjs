8227d97a6202701e76b7389564a9f30e8c1ec98b
hadi-azeez <hadi.azeez@nutrien.com.au>
2024-03-15 02:48:42 +0400

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,42 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
+
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()",
+          },
+          // Add other headers if necessary
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
